5a28674342da18dfa4462503cda360b9f8279be0
hippo <hippo.wu@coinflex.com>
2020-08-18 19:03:03 +0800

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,30 @@
-var gulp = require('gulp');
-var ghPages = require('gulp-gh-pages');
- 
-gulp.task('deploy', function() {
-  return gulp.src('./_book/**/*')
-    .pipe(ghPages());
-});
+const { src, dest, series, watch } = require("gulp");
+const plugins = require('gulp-load-plugins')();
+const ghPages = require('gulp-gh-pages');
+const htmlmin = require('gulp-htmlmin');
+
+const publish = () => {
+  return src('./_book/**/*')
+    .pipe(ghPages({
+      origin: 'origin',
+      branch: 'gh-pages'
+    }));
+};
+
+const minify = () => {
+  return src('./_book/**/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest('./_book/'));
+}
+const push = series(minify, publish);
+module.exports = {
+  minify,
+  push
+}
+// gulp.task('publish', () => {
+//   return gulp.src('./_book/**/*')
+//     .pipe(plugins.ghPages({
+//       origin: 'origin',
+//       branch: 'gh-pages'
+//     }));
+// });
